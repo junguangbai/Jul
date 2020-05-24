a116ba3b247359ad2a4aa33038d4334388ec5dc8
baijunguang <baijunguang@aliyun.com>
2020-05-24 13:07:48 +0800

    July is coming!

---
+++ src/com/atsh/java/Controller.java
@@ -0,0 +1,11 @@
+package com.atsh.java;
+
+/**
+ * @author baijunguang
+ * @date 2020/5/24-13:05
+ */
+public class Controller {
+    public static void main(String[] args) {
+        System.out.println("July is coming!!!");
+    }
+}
