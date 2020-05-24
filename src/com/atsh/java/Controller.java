e6f98e86cf9b1d03563a8fd4ee7ff4bbd303210c
JunGuangBai <junguangbai@aliyun.com>
2020-05-24 13:24:56 +0800

    增加求和方法

--- src/com/atsh/java/Controller.java
+++ src/com/atsh/java/Controller.java
@@ -8,5 +8,10 @@ public class Controller {
     public static void main(String[] args) {
         System.out.println("July is coming!!!");
         System.out.println("July is coming!!!,so hot ShangHai it is...");
+        System.out.println(add(3,5));
+    }
+    //增加求和方法
+    public static int add (int a,int b){
+        return a+b;
     }
 }
